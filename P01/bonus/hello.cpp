63d0ed21b91d957e3e17e3af88a1ddeb55f9c083
nirakarnepal <nirakar.us@gmail.com>
2019-08-28 16:28:16 -0500

    Bonus

---
+++ P01/bonus/hello.cpp
@@ -0,0 +1,15 @@
+//Nirakar Nepal
+//1001666002
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+   string name;
+   cout << "Please enter your name.  "<< endl;
+   cin >> name;
+   cout << "Hello " << name << "!" << endl;
+
+}
