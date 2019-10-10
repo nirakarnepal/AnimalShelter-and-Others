4816d88140e5c11d1d2e47e5abf3d7e254f6eaf7
nirakarnepal <nirakar.us@gmail.com>
2019-10-10 09:19:13 -0500

    still trying

--- P07/full_credit/main.cpp
+++ P07/full_credit/main.cpp
@@ -5,3 +5,4 @@ int main(int argc, char* argv[]) {
     Mainwin win;
     app->run(win);
 }
+
--- P07/full_credit/mainwin.h
+++ P07/full_credit/mainwin.h
@@ -9,25 +9,14 @@ class Mainwin : public Gtk::Window {
     void on_entry_activate();
     void on_button_click();
     void on_comboboxtext_activate();
-    void on_togglebutton_click();
-    void on_checkbutton_click();
-    void on_radiobutton_click(int button);
-    void on_spinbutton_value_changed();
-    void on_scale_value_changed();
+
   private:
     // These are the widgets demonstrated in order
     Gtk::Entry* entry;
     Gtk::ComboBoxText* comboboxtext;
     Gtk::Button* button;
     Gtk::ToggleButton* togglebutton;
-    Gtk::CheckButton* checkbutton;
-    Gtk::Frame* frame; // Contains the 2 radio buttons
-    Gtk::RadioButton* radiobutton1;
-    Gtk::RadioButton* radiobutton2; // need 2 to demo groups
-    Gtk::SpinButton* spinbutton;
-    Gtk::Label* label;
-    Gtk::Scale* scale;
-    Gtk::ProgressBar* progressbar;
+    
 
     Gtk::Button* dialog_button;
     void dialog();
